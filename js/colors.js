12c3df6aea747472600e7cb38cb73b97d602970b
Hantascu Alexandru <hantescu.alex@yahoo.com>
2016-09-20 22:33:25 +0300

    Added colors in colors.js

---
+++ js/colors.js
@@ -0,0 +1,14 @@
+var colors = {
+    "1": {
+        "culoare": "rosu",
+        "cod": "#FF0000"
+    },
+    "2": {
+        "culoare": "verde",
+        "cod": "#00FF00"
+    },
+    "3": {
+        "culoare": "galben",
+        "cod": "#FFFF00"
+    }
+}
